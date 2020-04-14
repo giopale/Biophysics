dcd12d5bedb6e74afa327c56910e8fe0f2254c7a
giopale <58114405+giopale@users.noreply.github.com>
2020-04-14 16:39:54 +0200

    prima prova

---
+++ ex1/test.m
@@ -0,0 +1,36 @@
+% CountParticles 
+% Author: Giorgio Palermo
+% V1.0
+% 
+% 
+% Syntax C=CountParticles(pos,L,M);
+% 
+% Input:
+% pos is a Nx3 array containing the positions of N
+% particles randomly distributed within the box.
+% L is the box side.
+% 
+% M is the number of voxels along one dimension.
+% 
+% Output:
+% C is a MxMxM matrix having the (i,j,k)-th element
+% equal to the number of particles within the (i,j,k)- 
+% th voxel of the box.
+clear
+
+N = 1e6; %number of particles
+L=1; %size of the box
+M = 1e2; %number of voxels
+pos = L*randn(N,3);
+
+tic
+C =CountParticles(pos,L,M);
+toc
+
+function C=CountParticles(pos,L,M)
+C = zeros(M,M,M);
+
+for i=1:M
+C(:,:,i) = histcounts2(pos(:,1),pos(:,2),M);
+end
+end
