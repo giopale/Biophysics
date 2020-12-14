c971faa83e3c3319b2395c3a1dba6f189c63261d
giopale <58114405+giopale@users.noreply.github.com>
2020-12-14 22:29:22 +0100

    Inizio delle conclusioni

--- Cell_Parameters/Test1.m
+++ Cell_Parameters/Test1.m
@@ -32,9 +32,10 @@ noise_amp= 1.5e-10;
 f10=figure(10);
 f10.Visible='off';
 clf
-pl1=plot(time,Vin);
-prop = {"Input voltage", "Time [s]", "Voltage [mV]","m","y"};
+pl1=plot(time*1e3,Vin*1e3,'Linewidth',1.5);
+prop = {"Input voltage", "Time [ms]", "Voltage [mV]","m","y"};
 SetPlot(get(gcf), prop)
+printpdf(f10,'Voltage_input');
 
 for i=1:1:Nstep
 	dVm=(-Vm(i)+Vin(i)*Rm/(Rm+Ra))/tau;
@@ -45,11 +46,27 @@ end
 f20 = figure(20);
 f20.Visible='off';
 clf
-pl2=plot(time,I1); %, time*10^3, I1*10^12)
+hold on
+grid on
+pl21=plot(time*1e3,I1*1e12);
+pl2=plot(time*1e3,I0*1e12,'r-','Linewidth',1.5);
 prop = {"Current", "Time [ms]"," Current [pA]","m","y"};
 SetPlot(get(gcf), prop)
 hold on
 disp('Saving plot... ')
+printpdf(f20,'Current')
+
+% f30 = figure(20);
+% f30.Visible='off';
+% clf
+% pl21=plot(time*1e3,I1*1e12); %, time*10^3, I1*10^12)
+% prop = {"Current", "Time [ms]"," Current [pA]","m","y"};
+% SetPlot(get(gcf), prop)
+% hold on
+% disp('Saving plot... ')
+% printpdf(f30,'Current_noisy')
+
+
 
 %Ra, Rm estimation:
 peak1=PeakEstim(time,I1,tstart,5); 
@@ -105,7 +122,7 @@ for i=1:1:Nstep+1
 end
 
 %Ra, Rm estimation:
-peak1=PeakEstim(time,I1r,tstart,5); 
+peak1=PeakEstim(time,I1r,tstart,5);
 I1r_infty_start = ceil((tstart+7*tau)/tstep);	%I1_infty estimation from data
 I1r_infty_end = ceil(tstop/tstep)-10;
 I1r_infty=mean(I1r(I1r_infty_start:I1r_infty_end));
@@ -116,21 +133,19 @@ Rm1r=Vin_amplitude-Vrest/I1r_infty;
 tau1r=0;
 Cm1r=0;
 
-f10=figure(30); %plot input voltage
-f10.Visible='off';
-clf
-pl1=plot(time,Vin);
-prop = {"Input voltage, resting pot.", "Time [s]", "Voltage [mV]","m","y"};
-SetPlot(get(gcf), prop)
-
 f40=figure(40); %plot solution for current
 f40.Visible='off';
 clf
-pl4=plot(time,I1r);
-prop = {"Current w. Resting Potential", "Time [s]", "Current [A]","m","y"};
-SetPlot(get(gcf), prop)
 hold on
 grid on
+pl21=plot(time*1e3,I1r*1e12);
+pl2=plot(time*1e3,I0r*1e12,'g-','Linewidth',1.7);
+tit="Current with Vr = " + string(-Vrest*1e3) +" mV";
+prop = {tit, "Time [ms]", "Current [pA]","m","y"};
+SetPlot(get(gcf), prop)
+disp('Saving plot... ')
+printpdf(f40,'Current_r')
+
 
 %Cm estimation via curve fitting
 xr= time(time>tstart & time<tstart+13*tau);	%Select right time interval
@@ -139,8 +154,16 @@ fitres1=fit(xr,yr,'exp1','startpoint',[1e-9,-1/0.2545e-3]); %fit specifying init
 tau1r=-1/fitres.b;
 Cm1r=tau1r*(1/Ra1r +1/Rm1r);
 y1r=fitres(xr)+I1r_infty;
-pl5=plot(xr,y1r,'r-',xr,yr+I1r_infty,'b.');
+f50=figure(50); %plot solution for current
+f50.Visible='off';
+clf
+hold on
+grid on
+pl5=plot(xr*1e3,y1r*1e12,'r-',xr*1e3,yr*1e12+I1r_infty,'b.');
+prop = {tit + "fit", "Time [ms]", "Current [pA]","m","y"};
+SetPlot(get(gcf), prop)
 disp('Saving plot... ')
+printpdf(f50,'Current_r_fitted')
 
 % Errors
 sigpt=noise_amp;
@@ -160,30 +183,38 @@ Cm1r_sig=Cm1r*sqrt((tau1r_sig/tau1r)^2 + (R_sig/R)^2);
 
 
 % Display some results on screen
-% disp('Caso base')
-% varnames={'I1_infty [pA]','Ra1 [MOhm]','Rm1 [MOhm]','tau1 [ms]','Cm1 [pf]'};
-% T=table([I1_infty,I1_infty_sig]*1e12, [Ra1,Ra1_sig]*1e-6, [Rm1,Rm1_sig]*1e-6,[tau1,tau1_sig]*1e3,[Cm1,Cm1_sig]*1e12,'VariableNames',varnames);
-% disp(T);
+disp('-------------- Caso base ---------------------------------')
+varnames={'I1_infty [pA]','Ra1 [MOhm]','Rm1 [MOhm]','tau1 [ms]','Cm1 [pf]'};
+T=table([I1_infty,I1_infty_sig]*1e12, [Ra1,Ra1_sig]*1e-6, [Rm1,Rm1_sig]*1e-6,[tau1,tau1_sig]*1e3,[Cm1,Cm1_sig]*1e12,'VariableNames',varnames);
+disp(T);
 
-% disp('Caso resting potential')
-% varnames={'I1r_infty [pA]','Ra1r [MOhm]','Rm1r [MOhm]','tau1r [ms]','Cm1r [pf]'};
-% Tr=table([I1r_infty,I1r_infty_sig]*1e12, [Ra1r,Ra1r_sig]*1e-6, [Rm1r,Rm1_sig]*1e-6,[tau1r,tau1r_sig]*1e3,[Cm1r,Cm1r_sig]*1e12,'VariableNames',varnames);
-% disp(Tr);
+disp('-------------- Caso resting potential --------------------')
+varnames={'I1r_infty [pA]','Ra1r [MOhm]','Rm1r [MOhm]','tau1r [ms]','Cm1r [pf]'};
+Tr=table([I1r_infty,I1r_infty_sig]*1e12, [Ra1r,Ra1r_sig]*1e-6, [Rm1r,Rm1_sig]*1e-6,[tau1r,tau1r_sig]*1e3,[Cm1r,Cm1r_sig]*1e12,'VariableNames',varnames);
+disp(Tr);
 
 
 % ------------------ Bessel filtering ------------------
 % Initializing structure fields
-npoints=5;
+npoints=6;
 
 signal(1).time=time;
 signal(1).I1=I1;
 signal(1).If=I1-I1;
-signal(1).base=0;
+signal(1).base=[0,0];
 signal(1).fcutoff=0;
+signal(1).Q=0;
+signal(1).tau=[0,0];
+signal(1).Cm=[0,0];
+signal(1).Ra=[0,0];
+signal(1).Rm=[0,0];
 signal(1).fig=figure('Visible',false);
 
+Qstart=tstart; %Transient charge integration limits
+Qend=tstart+3*tau;
+
 fcutoff=logspace(0,1,npoints)*1e3;
-disp('Filtering signals:')
+fprintf('Filtering %d signals: ', npoints)
 for i=1:1:npoints
 	signal(i).time=time;
 	signal(i).I1=I1;
@@ -196,28 +227,81 @@ for i=1:1:npoints
 	%fitting
 	time_peak=time(signal(i).If == max(signal(i).If));
 	tstart_fit=time_peak+0.1e-3;
-	signal(i).base=baseline(signal(i).If,tstart, tstop, tstep,tau);
-	[tauf,x, y, fitf]=fitthis(signal(i).time,signal(i).If,tstart_fit, tau, signal(i).base);
+	[base,base_sig]=baseline(signal(i).If,tstart, tstop, tstep,tau);
+	signal(i).base(1)=base;
+	signal(i).base(2)=base_sig;
+	[tauf,tauf_sig,x,y,fitf]=fitthis(signal(i).time,signal(i).If,tstart_fit, tau, signal(i).base(1));
+	signal(i).tau=[tauf,tauf_sig];
+	%Integrating charge
+	Qtransient=signal(i).If(signal(i).time>Qstart & signal(i).time<Qend);
+	Qtransient=(Qtransient-signal(i).base(1))*tstep;
+	signal(i).Q=sum(Qtransient);
+	%Computing parameters
+	num=signal(i).tau(1)*Vin_amplitude;
+	den=(signal(i).Q + signal(i).tau(1)*signal(i).base(1));
+	signal(i).Ra(1)=num/den; % Access resistance
+	num_sig=num*tauf_sig/tauf;
+	den_sig=sqrt(tauf^2*signal(i).base(2)^2 + signal(i).base(1)^2*tauf_sig^2);
+	signal(i).Ra(2)=signal(i).Ra(1)*sqrt((num_sig/num)^2+(den_sig/den)^2);
+	
+	signal(i).Rm(1)=Vin_amplitude/signal(i).base(1) - signal(i).Ra(1); %Membrane resistance
+	signal(i).Rm(2)=sqrt((Vin_amplitude*signal(i).base(2)/signal(i).base(1)^2)^2 + signal(i).Ra(2)^2);
+
+	RR=1/signal(i).Ra(1) +1/signal(i).Rm(1); %Membrane capacity
+	signal(i).Cm(1)=tauf*RR;
+	RR_sig=sqrt((signal(i).Ra(2)/signal(i).Ra(1)^2)^2 + (signal(i).Rm(2)/signal(i).Rm(1)^2)^2);
+	signal(i).Cm(2)=signal(i).Cm(1)*sqrt((tauf_sig/tauf)^2 + (RR_sig/RR)^2);
+
+
 
 	signal(i).fig=figure('Visible',false);
 	tit='Filtered signal: '+ string(round(fcutoff(i)/1e3,1)) +' kHz';
-	prop = {tit, "Time [s]", "Current [A]","m","y"};
+	prop = {tit, "Time [ms]", "Current [pA]","m","y"};
 	clf
-	plot(time,signal(i).If);
+	plot(time*1e3,signal(i).If*1e12);
 	hold on
-	plot(x,y,'b.',x,fitf,'r-');
+	plot(x*1e3,y*1e12,'b.',x*1e3,fitf*1e12,'r-');
 	SetPlot(get(gcf), prop);
 	grid on
 	filename='Bess'+string(ceil(fcutoff(i)))+'Hz';
 	printpdf(signal(i).fig,filename);
-	disp('plot '+string(i)+' over '+string(npoints) +' saved')
-end
+	fprintf('%d ',i)
 
-%Cm estimation via curve fitting
-% i=1;
-% tstart_fit=tstart+0.7e-3;
-% signal(i).base=baseline(signal(i).If,tstart, tstop, tstep,tau);
-% [tauf,x, y]=fitthis(signal(i).time,signal(i).If,tstart_fit, tau, signal(i).base);
+	%results
+	Cutfreq(i,1)=signal(i).fcutoff;
+	Racc(i,:)=signal(i).Ra;
+	Rmem(i,:)=signal(i).Rm;
+	Cmem(i,:)=signal(i).Cm;
+
+end
+	disp(' ')
+	varnames={'Cutoff freq.','Ra [MOhm]','Rm [MOhm]','Cm [pF]'};
+	T=table(Cutfreq,Racc/1e6,Rmem/1e6, Cmem*1e12,'VariableNames',varnames);
+	disp(T)
+
+
+% Signal plus filtered graphs;
+% f60=figure('Visible',false);
+f70=figure(70);
+f70.Visible='on';
+f70.Units = 'centimeters';
+f70.OuterPosition = [8 8 30 16];
+clf
+pl1=plot(time*1e3,signal(1).I1*1e12);
+pl1.DisplayName='No filtering';
+prop = {"Original vs. Filtered Signal", "Time [ms]", "Current [pA]","m","y"};
+SetPlot(get(gcf), prop)
+legend()
+hold on
+grid on
+for i=1:npoints
+	txt= string(round(signal(i).fcutoff/1e3,1)) + ' kHz cutoff';
+	plot(time*1e3,signal(i).If*1e12,'DisplayName',txt,'LineWidth',1.2)
+end
+hold off
+printpdf(f70,'Bess_all_freq')
+
+
 
 
 
@@ -312,19 +396,24 @@ function []=printpdf(fig_handle,filename)
 
 end
 
-function [I1_infty]=baseline(I1, tstart, tstop, tstep, tau)
+function [I1_infty, I1_infty_sig]=baseline(I1, tstart, tstop, tstep, tau)
 	I1_infty_start = ceil((tstart+5*tau)/tstep);	%I1_infty estimation from data
 	I1_infty_end = ceil(tstop/tstep)-10;
 	I1_infty = mean(I1(I1_infty_start:I1_infty_end));
+	how_many=sqrt(I1_infty_end-I1_infty_start);
+	I1_infty_sig=std(I1(I1_infty_start:I1_infty_end))/how_many;
 end
 
 
-function [tau1,x, y, fitfun]=fitthis(xdata, ydata, tstart_fit, tau, baseline)
+function [tau1,tau1_sig,x, y, fitfun]=fitthis(xdata, ydata, tstart_fit, tau, baseline)
 	x= xdata(xdata>tstart_fit & xdata<tstart_fit+4*tau);	%Select right time interval
 	y= ydata(xdata>tstart_fit & xdata<tstart_fit+4*tau) - baseline; %subtract baseline
 	fitres=fit(x,y,'exp1','startpoint',[1e-9,-1/0.2545e-3]); %fit specifying initial parameters
 	tau1=-1/fitres.b;
-	% Cm1=tau1*(1/Ra1 +1/Rm1);
+	bounds=confint(fitres);
+	bounds=-(1./bounds);
+	bounds(:,1)=[];
+	tau1_sig=(bounds(2)-bounds(1))/2;
 	y=y+baseline;
 	fitfun=fitres(x)+baseline;
 	% figure(666)
